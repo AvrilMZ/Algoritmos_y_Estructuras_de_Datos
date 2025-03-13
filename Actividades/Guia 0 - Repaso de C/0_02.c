a97a23248db29de397bdb9bb67901b383dbd769a
AvrilMZ <amorfeo@fi.uba.ar>
2025-03-12 23:44:12 -0300

    Completo

--- Actividades/Guia 0 - Repaso de C/0_02.c
+++ Actividades/Guia 0 - Repaso de C/0_02.c
@@ -1,3 +1,10 @@
+/*
+ * Ahora, les damos la firma de una función que recibe un vector, su tope y
+ * la letra a buscar. Devolver en dónde se encuentra la letra o -1 si no se
+ * encuentra en el vector. Para seguir las buenas prácticas, ya les definimos la
+ * variable ERROR en solucion.h ;)
+ */
+
 #include "solucion.h"
 
 /*
@@ -6,5 +13,13 @@
  */
 int posicion_letra(char vector[MAX_VECTOR], int tope, char letra)
 {
-	// tu código
+	bool letra_encontrada = false;
+	int indice_encontrado = ERROR;
+	for (int i = 0; i < tope && !letra_encontrada; i++) {
+		if (vector[i] == letra) {
+			letra_encontrada = true;
+			indice_encontrado = i;
+		}
+	}
+	return indice_encontrado;
 }
