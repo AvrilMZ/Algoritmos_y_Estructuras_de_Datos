69bc8f1283746ebbe803de3dc6c16b66a20ce6af
AvrilMZ <amorfeo@fi.uba.ar>
2025-03-20 21:03:49 -0300

    Se arreglan los errores de los test de entrega

--- TP0/src/archivo.c
+++ TP0/src/archivo.c
@@ -1,10 +1,10 @@
 #include "archivo.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MODO_LECTURA "r"
-
-const int TAMANO_CONST_BUFFER = 10;
+const int CAPACIDAD_INICIAL = 16;
 
 struct Archivo {
 	FILE *file;
@@ -19,27 +19,53 @@ struct Archivo {
  */
 Archivo *archivo_abrir(const char *nombre)
 {
-	if (nombre == NULL) {
-		printf("No se puedo abrir el archivo (null)\n");
+	if (!nombre || !*nombre) {
+		printf("No se pudo abrir el archivo\n");
 		return NULL;
 	}
 
 	Archivo *archivo = malloc(sizeof(Archivo));
-	if (archivo == NULL) {
+	if (!archivo) {
 		printf("Error reservando memoria\n");
 		return NULL;
 	}
 
 	archivo->file = fopen(nombre, MODO_LECTURA);
-	if (archivo->file == NULL) {
-		printf("No se puedo abrir el archivo %s\n", nombre);
+	if (!archivo->file) {
 		free(archivo);
+		printf("No se pudo abrir el archivo %s\n", nombre);
 		return NULL;
 	}
+
 	archivo->lineas_leidas = 0;
 	archivo->ultima_linea = NULL;
-
 	return archivo;
+}
+
+// Reserva memoria para la línea y la inicializa.
+char *reservar_memoria_linea(int capacidad)
+{
+	char *linea = malloc(sizeof(char) * (size_t)capacidad);
+	if (!linea) {
+		printf("Error reservando memoria\n");
+		return NULL;
+	}
+	return linea;
+}
+
+// Realoca memoria para la línea y los bytes adicionales (si los hay) se inicializan en 0.
+char *realocar_memoria_linea(char *linea, int capacidad)
+{
+	int capacidad_anterior = capacidad / 2;
+	char *nueva_linea = realloc(linea, sizeof(char) * (size_t)capacidad);
+	if (!nueva_linea) {
+		printf("Error realocando memoria\n");
+		return NULL;
+	}
+	memset(nueva_linea + capacidad_anterior, 0,
+	       (size_t)(capacidad - capacidad_anterior));
+
+	return nueva_linea;
 }
 
 /*
@@ -49,7 +75,7 @@ Archivo *archivo_abrir(const char *nombre)
  */
 const char *archivo_leer_linea(Archivo *archivo)
 {
-	if (archivo == NULL || archivo->file == NULL) {
+	if (!archivo || !archivo->file) {
 		return NULL;
 	}
 
@@ -58,41 +84,32 @@ const char *archivo_leer_linea(Archivo *archivo)
 		archivo->ultima_linea = NULL;
 	}
 
+	if (feof(archivo->file) != 0) {
+		return NULL;
+	}
+
+	int capacidad = CAPACIDAD_INICIAL;
+	char *linea = reservar_memoria_linea(capacidad);
+	if (!linea) {
+		return NULL;
+	}
+
 	int caracter = fgetc(archivo->file);
-	if (caracter == EOF) {
-		return NULL;
-	}
-
-	char *linea = malloc(sizeof(char) * (size_t)TAMANO_CONST_BUFFER);
-	if (linea == NULL) {
-		printf("Error reservando memoria\n");
-		return NULL;
-	}
-
-	int tamano_actual_buffer = TAMANO_CONST_BUFFER;
 	int chars_leidos = 0;
-	while (caracter != '\n' && caracter != EOF) {
-		if (chars_leidos + 1 >= tamano_actual_buffer) {
-			tamano_actual_buffer += TAMANO_CONST_BUFFER;
+	while (caracter != '\n' && feof(archivo->file) == 0) {
+		if (chars_leidos + 1 >= capacidad) {
+			capacidad *= 2;
 			char *nueva_linea =
-				realloc(linea, (size_t)tamano_actual_buffer);
-			if (nueva_linea == NULL) {
-				printf("Error reservando memoria\n");
+				realocar_memoria_linea(linea, capacidad);
+			if (!nueva_linea) {
 				free(linea);
-				if (archivo->ultima_linea) {
-					free(archivo->ultima_linea);
-					archivo->ultima_linea = NULL;
-				}
+				linea = NULL;
 				return NULL;
 			}
 			linea = nueva_linea;
 		}
 		linea[chars_leidos++] = (char)caracter;
 		caracter = fgetc(archivo->file);
-	}
-
-	if (chars_leidos == 0 && caracter == '\n') {
-		linea[chars_leidos++] = '\0';
 	}
 
 	linea[chars_leidos] = '\0';
@@ -108,23 +125,10 @@ const char *archivo_leer_linea(Archivo *archivo)
  */
 int archivo_hay_mas_lineas(Archivo *archivo)
 {
-	if (archivo == NULL || archivo->file == NULL) {
+	if (!archivo || !archivo->file) {
 		return 0;
 	}
-
-	long posicion_actual = ftell(archivo->file);
-	if (posicion_actual == -1) {
-		printf("Error al obtener la posición del archivo\n");
-		return 0;
-	}
-
-	int caracter = fgetc(archivo->file);
-	if (caracter == EOF) {
-		return 0;
-	}
-
-	fseek(archivo->file, posicion_actual, SEEK_SET);
-	return 1;
+	return !feof(archivo->file);
 }
 
 /*
@@ -134,7 +138,7 @@ int archivo_hay_mas_lineas(Archivo *archivo)
  */
 int archivo_lineas_leidas(Archivo *archivo)
 {
-	if (archivo == NULL) {
+	if (!archivo) {
 		return 0;
 	}
 	return archivo->lineas_leidas;
@@ -145,13 +149,16 @@ int archivo_lineas_leidas(Archivo *archivo)
  */
 void archivo_cerrar(Archivo *archivo)
 {
-	if (archivo != NULL) {
-		if (archivo->file != NULL) {
+	if (archivo) {
+		if (archivo->file) {
 			fclose(archivo->file);
+			archivo->file = NULL;
 		}
 		if (archivo->ultima_linea) {
 			free(archivo->ultima_linea);
+			archivo->ultima_linea = NULL;
 		}
 		free(archivo);
+		archivo = NULL;
 	}
 }
