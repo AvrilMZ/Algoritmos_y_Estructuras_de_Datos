6ee8e85ad3d8e9e23e6c2af2c5963fc8386af055
AvrilMZ <amorfeo@fi.uba.ar>
2025-03-24 22:22:36 -0300

    Estetica de comentarios

--- TP1/src/pokedex.h
+++ TP1/src/pokedex.h
@@ -27,7 +27,7 @@ struct pokemon {
 
 typedef struct pokedex pokedex_t;
 
-/**
+/*
  * Crea una nueva pokedex a partir de un archivo.
  * 
  * Devuelve un puntero a la pokedex si se pudo abrir el archivo, o NULL en caso contrario.
@@ -35,33 +35,31 @@ typedef struct pokedex pokedex_t;
  * Cada pokemon en el archivo debe tener el siguiente formato: id;nombre;tipo;fuerza;destreza;inteligencia
  * 
  * Al encontrar un pokemon inválido, se ignora y finaliza la lectura del archivo. Se leen todos los pokemon válidos del archivo y se almacenan en la pokedex. Si no se pudo leer ningún pokemon válido, se devuelve NULL.
- * 
-
-*/
+ */
 pokedex_t *pokedex_abrir(const char *archivo);
 
-/**
+/*
  * Devuelve la cantidad de pokemones en la pokedex.
-*/
+ */
 unsigned pokedex_cantidad_pokemones(pokedex_t *pokedex);
 
-/**
+/*
  * Busca un pokemon con el nombre especificado en la pokedex.
  * 
  * Devuelve un puntero al pokemon si se encontró, o NULL en caso contrario.
-*/
+ */
 const struct pokemon *pokedex_buscar_pokemon_nombre(pokedex_t *pokedex,
 						    const char *nombre);
 
-/**
+/*
  * Busca un pokemon con el id especificado en la pokedex.
  * 
  * Devuelve un puntero al pokemon si se encontró, o NULL en caso contrario.
-*/
+ */
 const struct pokemon *pokedex_buscar_pokemon_id(pokedex_t *pokedex,
 						unsigned id);
 
-/**
+/*
  * Invoca la función especificada para cada pokemon en la pokedex.
  * 
  * La función se debe invocar con los pokemon ordenados según el criterio de iteracion dado (creciente por nombre o por id).
@@ -72,14 +70,14 @@ const struct pokemon *pokedex_buscar_pokemon_id(pokedex_t *pokedex,
  * Devuelve la cantidad de pokemones iterados.
  * 
  * Restriccion del TP: implementar con complejidad O(n) en tiempo y O(1) en espacio.
-*/
+ */
 unsigned pokedex_iterar_pokemones(pokedex_t *pokedex, enum modo_iteracion modo,
 				  bool (*funcion)(struct pokemon *, void *),
 				  void *ctx);
 
-/**
+/*
  * Destruye la pokedex.
-*/
+ */
 void pokedex_destruir(pokedex_t *pokedex);
 
 #endif // POKEDEX_H
