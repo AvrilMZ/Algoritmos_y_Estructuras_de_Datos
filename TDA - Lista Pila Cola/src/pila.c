b4246b855af9b8896963b7a609c0631f41243ca2
AvrilMZ <amorfeo@fi.uba.ar>
2025-04-09 18:28:45 -0300

    Implemento pila_crear

--- TDA - Lista Pila Cola/src/pila.c
+++ TDA - Lista Pila Cola/src/pila.c
@@ -5,8 +5,32 @@ struct pila {
 	lista_t *lista;
 };
 
+// Reserva memoria para un struct de tipo 'pila_t'.
+pila_t *reservar_memoria_pila()
+{
+	pila_t *pila = calloc(1, sizeof(pila_t));
+	if (!pila) {
+		return NULL;
+	}
+	return pila;
+}
+
 pila_t *pila_crear()
 {
+	pila_t *pila = reservar_memoria_pila();
+	if (!pila) {
+		return NULL;
+	}
+
+	lista_t *lista_interna = lista_crear();
+	if (!lista_interna) {
+		free(pila);
+		pila = NULL;
+		return NULL;
+	}
+	pila->lista = lista_interna;
+
+	return pila;
 }
 
 bool pila_apilar(pila_t *, void *elemento)
