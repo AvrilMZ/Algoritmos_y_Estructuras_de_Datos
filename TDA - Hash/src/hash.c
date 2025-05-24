aab7ecd10be7d32cf5f37ff2250911ca08a7c4dc
Avril <amorfeo@fi.uba.ar>
2025-05-24 16:13:33 -0300

    Commit inicial

---
+++ TDA - Hash/src/hash.c
@@ -0,0 +1,44 @@
+#include "hash.h"
+
+struct hash {};
+
+hash_t *hash_crear(size_t capacidad_inicial)
+{
+}
+
+hash_t *hash_crear_con_funcion(size_t capacidad_inicial,
+			       size_t (*f)(const char *))
+{
+}
+
+bool hash_insertar(hash_t *h, const char *clave, void *valor, void **anterior)
+{
+}
+
+void *hash_sacar(hash_t *h, const char *clave)
+{
+}
+
+void *hash_buscar(hash_t *h, const char *clave)
+{
+}
+
+bool hash_existe(hash_t *h, const char *clave)
+{
+}
+
+size_t hash_tamanio(hash_t *h)
+{
+}
+
+void hash_destruir(hash_t *h)
+{
+}
+
+void hash_destruir_todo(hash_t *h, void (*destructor)(void *))
+{
+}
+
+size_t hash_iterar_claves(hash_t *h, bool (*f)(const char *, void *), void *ctx)
+{
+}
